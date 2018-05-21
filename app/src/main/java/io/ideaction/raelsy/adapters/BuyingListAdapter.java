da4e824ce0244b127158f21162ba4fc6ae440e91
eyalmnm <eyal@em-projects.com>
2018-05-21 19:14:56 +0300

    Change the Buying Tab lists to expandable list.

--- app/src/main/java/io/ideaction/raelsy/adapters/BuyingListAdapter.java
+++ app/src/main/java/io/ideaction/raelsy/adapters/BuyingListAdapter.java
@@ -1,59 +1,119 @@
 package io.ideaction.raelsy.adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import io.ideaction.raelsy.R;
 import io.ideaction.raelsy.models.BuyingItem;
 
-public class BuyingListAdapter extends BaseAdapter {
+public class BuyingListAdapter extends BaseExpandableListAdapter {
     private static final String TAG = "BuyingListAdapter";
 
-    private ArrayList<BuyingItem> buyingItems;
     private Context context;
     private LayoutInflater inflater;
+    private ArrayList<ArrayList<BuyingItem>> buyingItems;
 
-    public BuyingListAdapter(Context context, ArrayList<BuyingItem> buyingItems) {
+    public BuyingListAdapter(Context context, ArrayList<ArrayList<BuyingItem>> buyingItems) {
         inflater = LayoutInflater.from(context);
         this.buyingItems = buyingItems;
         this.context = context;
     }
 
-    public ArrayList<BuyingItem> getBuyingItems() {
-        return buyingItems;
-    }
-
-    public void setBuyingItems(ArrayList<BuyingItem> buyingItems) {
-        this.buyingItems = buyingItems;
-    }
-
-
     @Override
-    public int getCount() {
+    public int getGroupCount() {
         return buyingItems.size();
     }
 
     @Override
-    public Object getItem(int i) {
+    public int getChildrenCount(int i) {
+        return buyingItems.get(i).size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
         return buyingItems.get(i);
     }
 
     @Override
-    public long getItemId(int i) {
+    public Object getChild(int groupPosition, int childPosition) {
+        return buyingItems.get(groupPosition).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int i) {
         return i;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        if (null == view) {
+            view = inflater.inflate(R.layout.layout_buying_header, null);
+        }
+        TextView buyingTagListsHeader = view.findViewById(R.id.buyingTagListsHeader);
+        int stringId = (0 == 1) ? R.string.buying_tab_shared_string : R.string.buying_tab_favorit_string;
+        buyingTagListsHeader.setText(stringId);
+        return view;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean b, View view, ViewGroup viewGroup) {
         if (null == view) {
             view = inflater.inflate(R.layout.layout_buying_item, null);
         }
 
+        // Invoke references to UI components
+        LinearLayout imagesContainer = view.findViewById(R.id.imagesContainer);
+        TextView buyingItemPrice = view.findViewById(R.id.buyingItemPrice);
+        TextView buyingItemFullAddress = view.findViewById(R.id.buyingItemFullAddress);
+        TextView buyingItemFullDetails = view.findViewById(R.id.buyingItemFullDetails);
+        ImageView buyingItemLikeImageView = view.findViewById(R.id.buyingItemLikeImageView);
+
+        // Init UI
+        final BuyingItem buyingItem = buyingItems.get(groupPosition).get(childPosition);
+        buyingItemPrice.setText("$" + String.valueOf(buyingItem.getPrice()));
+        buyingItemFullAddress.setText(context.getString(R.string.buying_item_address_full_address, buyingItem.getAddress(),
+                buyingItem.getCity(), buyingItem.getState(), buyingItem.getZipCode()));
+        buyingItemFullDetails.setText(context.getString(R.string.buying_item_property_details, buyingItem.getBedRooms(),
+                buyingItem.getBathRooms(), buyingItem.getApartmentArea()));
+        int likeDrawableId = buyingItem.isLiked() ? R.drawable.like_fill : R.drawable.like_empty;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            buyingItemLikeImageView.setImageDrawable(context.getDrawable(likeDrawableId));
+        } else {
+            buyingItemLikeImageView.setImageDrawable(context.getResources().getDrawable(likeDrawableId));
+        }
+        buyingItemLikeImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buyingItem.setLiked(! buyingItem.isLiked());
+                BuyingListAdapter.this.notifyDataSetChanged();
+            }
+        });
         return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
     }
 }
--- app/src/main/java/io/ideaction/raelsy/screens/fragments/BuyingTabFragment.java
+++ app/src/main/java/io/ideaction/raelsy/screens/fragments/BuyingTabFragment.java
@@ -2,11 +2,14 @@ package io.ideaction.raelsy.screens.fragments;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -19,12 +22,10 @@ public class BuyingTabFragment extends Fragment {
     private static final String TAG = "BuyingTabFragment";
 
     // UI Components
-    private ListView buyingListView;
-    private ArrayList<BuyingItem> sharedBuyingItems;
-    private BuyingListAdapter sharedItemsAdapter;
-    private ArrayList<BuyingItem> favoriteBuyingItems;
-    private BuyingListAdapter favoriteItemsAdapter;
+    private ExpandableListView buyingPropertiesExpandableListView;
 
+    private ArrayList<ArrayList<BuyingItem>> propertiesLists;
+    private BuyingListAdapter adapter;
     private Context context;
 
     @Nullable
@@ -37,6 +38,53 @@ public class BuyingTabFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ccc
+
+        propertiesLists = new ArrayList<>(2);
+
+        // Invoke references to UI components
+        buyingPropertiesExpandableListView = view.findViewById(R.id.buyingPropertiesExpandableListView);
+
+        // Init Buying List
+        adapter = new BuyingListAdapter(context, propertiesLists);
+        buyingPropertiesExpandableListView.setAdapter(adapter);
+
+        // Expand all children
+        for(int i=0; i < adapter.getGroupCount(); i++) {
+            buyingPropertiesExpandableListView.expandGroup(i);
+        }
+
+        // Make the list un - collapsible
+        buyingPropertiesExpandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,int  groupPosition, long id) {
+                return true;
+            }
+        });
+
+        // Check permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!checkPermissions()) {
+                requestPermission();
+            } else {
+                continueAppLoading();
+            }
+        } else {
+            continueAppLoading();
+        }
+
+    }
+
+    private void continueAppLoading() {
+        ArrayList<BuyingItem> sharedProperties = getSharedProperties();
+        ArrayList<BuyingItem> favoriteProperties = getFavoriteProperties();
+        propertiesLists.clear();
+        propertiesLists.add(0, sharedProperties);
+        propertiesLists.add(1, favoriteProperties);
+        new Handler(getActivity().getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
